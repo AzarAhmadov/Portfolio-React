1bdcc1019903b2c0cd822846c3b9b0238d82b435
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-02-25 11:21:51 -0800

    .

--- src/components/home/PortfolioCards.jsx
+++ src/components/home/PortfolioCards.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function PortfolioCards({ elem }) {
     return (
         <div className='portfolio-item'>
-            <img src={elem.img} alt="" />
+            <div className="portfolio-img">
+                <img src={elem.img} alt="" />
+            </div>
             <span className="portfolio-name">{elem.name}</span>
             <div className="links">
                 <a target='_blank' href={elem.github}> Github </a>
