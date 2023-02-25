02169a00e2bbdc20295149ba8b3e1b76d7b93114
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-02-25 14:28:37 -0800

    .

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -28,9 +28,9 @@ const About = ({ className, ...rest }) => {
                     </div>
                     <div className="col-lg-6 about-col-right">
                         <div data-aos="fade-left" className="about-right-item">
-                            <span className='about-text'>
+                            <p className='about-text'>
                                 I am living in Baku. I like Information technologies and search Informasion technologies. I am open to new things and I like to work on myself. I am very hardworking.
-                            </span>
+                            </p>
                             <ul>
                                 <li>
                                     <span className='d-flex flex-column' data-number="2">
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -20,9 +20,9 @@ const Main = () => {
                             <div data-aos='fade-left' className="right-text">
                                 <h1 className='title'> Hi, I'm Azar Ahmadov </h1>
                                 <span className='work'> Front-end developer </span>
-                                <small className='work-text'>
+                                <p className='work-text'>
                                     I have a lot of knowledge and experience <br /> in this field
-                                </small>
+                                </p>
                                 <a className='global-button' href="https://api.whatsapp.com/send?phone=9940558210637"> Contact me  <i className="fa-regular fa-id-badge"></i></a>
                             </div>
                         </div>
