5995093c641489ddf82e99204ff56879a0dcc7f3
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-02-26 23:21:58 -0800

    .

--- src/components/home/PortfolioCards.jsx
+++ src/components/home/PortfolioCards.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export default function PortfolioCards({ elem }) {
     return (
         <div className='portfolio-item'>
             <div className="portfolio-img">
-                <img src={elem.img} alt="" />
+                <LazyLoadImage src={elem.img}
+                    alt="Image Alt"
+                />
             </div>
             <span className="portfolio-name">{elem.name}</span>
             <div className="links">
-                <a  target='_blank' href={elem.github}> Github <i style={{ fontSize: "22px" }} className="fa-brands fa-github"></i> </a>
-                <a  target='_blank' href={elem.demo}> Demo <i className="fa-solid fa-chevron-right"></i> </a>
+                <a target='_blank' href={elem.github}> Github <i style={{ fontSize: "22px" }} className="fa-brands fa-github"></i> </a>
+                <a target='_blank' href={elem.demo}> Demo <i className="fa-solid fa-chevron-right"></i> </a>
             </div>
         </div>
     )
